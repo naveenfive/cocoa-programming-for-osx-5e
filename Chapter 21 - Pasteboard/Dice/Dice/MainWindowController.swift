8dc57dce4c75f1bc9d03d6ac980f4e6c31cab120
Naveen <nchegireddy@gmail.com>
2018-03-07 12:09:02 -0500

    chapter 21 update

--- Chapter 21 - Pasteboard/Dice/Dice/MainWindowController.swift
+++ Chapter 21 - Pasteboard/Dice/Dice/MainWindowController.swift
@@ -9,14 +9,14 @@
 import Cocoa
 
 class MainWindowController: NSWindowController {
-
-	override var windowNibName: String! {
-		return "MainWindowController"
-	}
-	
+    
+    override var windowNibName: NSNib.Name! {
+        return NSNib.Name(rawValue: "MainWindowController")
+    }
+    
     override func windowDidLoad() {
         super.windowDidLoad()
-
+        
         // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
     }
     
